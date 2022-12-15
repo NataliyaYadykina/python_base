7a8eea281b3cc31bca2dfada590e5f6b6b13b7d8
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-12-15 12:15:56 +0300

    Add example 01 -  homework 1

---
+++ homework/hw1/ex01.py
@@ -0,0 +1,16 @@
+# Íàïèøèòå ïğîãğàììó, êîòîğàÿ ïğèíèìàåò íà âõîä öèôğó, îáîçíà÷àşùóş äåíü íåäåëè, 
+# è ïğîâåğÿåò, ÿâëÿåòñÿ ëè ıòîò äåíü âûõîäíûì.
+# Ïğèìåğ:
+# - 6 -> äà
+# - 7 -> äà
+# - 1 -> íåò
+
+day_number = int(input('Write day number: '))
+
+if day_number > 0 and day_number < 8:
+    if day_number == 6 or day_number == 7:
+        print('Weekend!!! :)')
+    else:
+        print('Work!')
+else:
+    print('Wrong number!')
