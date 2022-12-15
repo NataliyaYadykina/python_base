8025ac98a7166a1f5365aecbd1d91ff81a4f4ffc
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2022-12-15 13:34:33 +0300

    Add example 02 - homework 1

---
+++ homework/hw1/ex02.py
@@ -0,0 +1,17 @@
+﻿# (!!!Доп!!!) Напишите программу для проверки истинности утверждения 
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+# (0,0,0), (0,0,1) и тд.
+
+my_set = [0, 1]
+
+for i in my_set:
+    for j in my_set:
+        for k in my_set:
+            if (not(my_set[i] or my_set[j] or my_set[k])) == (not my_set[i] and not my_set[j] and not my_set[k]):
+                print('True')
+                
+            else:
+                print('False')
+            print(f'x = {my_set[i]} y = {my_set[j]} z = {my_set[k]}')
+            print(f'{not(my_set[i] or my_set[j] or my_set[k])} = {not my_set[i] and not my_set[j] and not my_set[k]}')
+            print()
