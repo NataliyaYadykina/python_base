02c8dd0a3f34c674488e49d4e42e26abd3fe8040
NataliyaYadykina <115035865+NataliyaYadykina@users.noreply.github.com>
2023-01-21 15:55:35 +0300

    Update ex02.py

--- seminars/sem5/ex02.py
+++ seminars/sem5/ex02.py
@@ -1,1 +1,13 @@
-# Напишите программу, удаляющую из текста все слова, содержащие ""абв"".text = 'abchgbdsh hfjbd kjnw jfhdabc'text_del = 'abc'text_lst = text.split(' ')new_text = ''for i in range(len(text_lst) - 1):    if text_del not in text_lst[i]:        new_text = new_text + text_lst[i] + ' 'print(new_text)
+# РќР°РїРёС€РёС‚Рµ РїСЂРѕРіСЂР°РјРјСѓ, СѓРґР°Р»СЏСЋС‰СѓСЋ РёР· С‚РµРєСЃС‚Р° РІСЃРµ СЃР»РѕРІР°, СЃРѕРґРµСЂР¶Р°С‰РёРµ ""Р°Р±РІ"".
+
+text = 'abchgbdsh hfjbd kjnw jfhdabc'
+text_del = 'abc'
+
+text_lst = text.split(' ')
+
+new_text = ''
+for i in range(len(text_lst) - 1):
+    if text_del not in text_lst[i]:
+        new_text = new_text + text_lst[i] + ' '
+
+print(new_text)
