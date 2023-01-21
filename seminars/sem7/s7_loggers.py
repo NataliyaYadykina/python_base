259025e7dae856717633bb2293579c9af60095a0
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2023-01-21 21:13:32 +0300

    Add sem1

---
+++ seminars/sem7/s7_loggers.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+def logger(data, file_path = 's7_log.csv', func_name = None):
+    time = datetime.now()
+    with open(file_path, 'a', encoding = 'utf-8') as f:
+        f.write(f'{time}; {func_name}; {data}\n')
